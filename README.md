dd06df9a8a4fe480ae543fba2432af2b9706f8ca
NaranjoJimenezAndres <andresgassgass@gmail.com>
2024-03-03 20:36:23 +0100

    custom hooks

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+customHooks
+- debe empezar por 'use'
+- no debe estar condicionado (if/else)
+
+
+
+NO se pueden lanzar useStates anidados por ahi perdidos a otras funciones
--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,33 @@ const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/';
 
 function App() {
   const [fact, setFact] = useState('');
-  const [ imageUrl, setImageUrl ] = useState('');
+  const { imageUrl } = useCatImage({ fact });
 
   useEffect(() => {
     getRamdomFact().then((fact) => setFact(fact));
   }, [])
 
-
-  useEffect(() => {
-    if (!fact) return;
+// CUSTOM HOOKS
+  function useCatImage ({ fact}) {
+    const [ imageUrl, setImageUrl ] = useState('');
     
-    const threeFirstWord = fact.split(' ', 3).join(' ');
-    console.log(threeFirstWord);
+    //efecto que se ejecuta cuando cambia el valor de fact
+    useEffect(() => {
+      if (!fact) return;
+    
+      const threeFirstWord = fact.split(' ', 3).join(' ');
+      console.log(threeFirstWord);
 
-    fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
-      .then((response) => response.json())
-      .then(data => {
-        const { url } = data;
-          setImageUrl(url)
-        })
-      }, [fact])
+      fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
+        then((response) => response.json())
+        .then(data => {
+          const { url } = data;
+            setImageUrl(url)
+          })
+        }, [fact])
+
+        return { imageUrl }; //se retorna la imagen
+  }
 
   const handleGetNewFact = async () => {
     const newFact = await getRamdomFact();
